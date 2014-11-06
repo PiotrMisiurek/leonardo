f66c1b9b30eaa4496f50498ac966b8666df2a229
Piotr Misiurek <p.misiurek@gmail.com>
2014-11-06 01:04:35 +0100

    project#choice and project.choice

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ActiveRecord::Base
+
+  def choice
+    { name => weight }
+  end
+
+  def self.choice
+    choice = {}
+    self.all.each {|project| choice.merge!(project.choice) }
+    choice
+  end
 end
