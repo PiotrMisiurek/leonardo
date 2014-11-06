57e77b252d01837ea88d60cf9f6039d51b2cf9dd
Piotr Misiurek <p.misiurek@gmail.com>
2014-11-06 01:13:31 +0100

    Routes: set root to projects#choose

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'projects#index'
+  root 'projects#choose'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
