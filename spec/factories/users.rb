ba06e70a6f6b107e7c93d7b5447f865782a20d97
Piotr Misiurek <p.misiurek@gmail.com>
2014-11-08 22:49:41 +0100

    Fill in users factory with data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
+  sequence(:email) { |n| "user_#{n}@example.com" }
+  sequence(:password, aliases: [:password_confirmation]) { |n| 'passwo_rd' }
+
   factory :user do
+    email
+    password
+    password_confirmation
   end
 end
