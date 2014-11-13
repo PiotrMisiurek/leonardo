d3a5f4852b39ee0704927afedf9b25bf1cd9a9f0
Piotr Misiurek <p.misiurek@gmail.com>
2014-11-13 17:02:26 +0100

    Test to be sure ProjectChooser choose only projects from collection it got

--- spec/services/project_chooser_spec.rb
+++ spec/services/project_chooser_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe ProjectChooser do
-  let!(:project) { create(:project) }
-  let!(:project_chooser) { ProjectChooser.new(Project.all) }
+  let!(:user) { create(:user) }
+  let!(:project) { create(:project, user: user) }
+  let!(:project_chooser) { ProjectChooser.new(user.projects) }
 
   it { expect { project_chooser.choose }.to change{ project.working_hours.count }.by(1) }
+
+  describe 'choose project only from collection that got in params' do
+    let!(:empty_user) { create(:user) }
+    it { expect(ProjectChooser.new(empty_user.projects).choose).to eq(nil) }
+  end
 end
